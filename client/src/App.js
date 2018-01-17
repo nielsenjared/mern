3911e216bd668c16f57dedc0f2ff8465764fc74e
Jared Nielsen <jared@jarednielsen.com>
2018-01-17 17:53:25 -0500

    stormy beach, 74196

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello! My name is Stormy Beach</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          My favorite number is <code>74196</code>.
         </p>
       </div>
     );
